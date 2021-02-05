4a482aeec2558f0ed1e334045ddc76fb56cdfe95
GBShadow <gbs.shadow@gmail.com>
2021-02-04 23:25:39 -0300

    Routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
+import Hero from './pages/Hero';
+import SaveHero from './pages/SaveHero';
+import FindHeroes from './pages/FindHeroes';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Landing} />
+      <Route path="/hero" component={Hero} />
+      <Route path="/save-hero" component={SaveHero} />
+      <Route path="/find-heroes" component={FindHeroes} />
     </BrowserRouter>
   );
 };
