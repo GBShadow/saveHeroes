7a16b56af94abc975dcc0f12539e9a11c016df4b
GBShadow <gbs.shadow@gmail.com>
2021-02-04 23:25:32 -0300

    Page Hero

---
+++ frontend/src/pages/Hero/index.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/images/logo.png';
+import api from '../../services/api';
+
+import './styles.css';
+
+interface Hero {
+  id: number;
+  name: string;
+  short_description: string;
+  complete_description: string;
+  url_image: string;
+}
+
+interface MatchParams {
+  match: {
+    params: {
+      id: number;
+    };
+  };
+}
+
+const Hero: React.FC<MatchParams> = ({ match }) => {
+  const [hero, setHero] = useState<Hero>({} as Hero);
+
+  const { id } = match.params;
+
+  useEffect(() => {
+    api.get(`/heroes/${id}`).then(response => {
+      console.log(response.data);
+      setHero(response.data);
+    });
+  }, [hero]);
+
+  return (
+    <div id="page-hero">
+      <div className="logo-container">
+        <img src={logoImg} alt="Save Heroes" />
+      </div>
+
+      <main>
+        <div className="hero-content-container">
+          <div className="hero-container">
+            <div className="hero-avatar-container">
+              <img src={hero.url_image} alt={hero.name} />
+            </div>
+            <div className="text-content">
+              <strong>{hero.name}</strong>
+              <span>{hero.short_description}</span>
+              <p>
+                <strong>Descrição: </strong>
+                {hero.complete_description}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="button-container">
+          <Link to="/find-heroes" className="back-button">
+            Voltar
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Hero;
